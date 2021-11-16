9b6f4293541050a52f2e60b0698a7dc1921d9fb4
Jihoun Im <jim@wustl.edu>
2021-11-15 19:47:25 -0600

    pushing changes for firebase spotify userURI

---
+++ arpeggio/Controllers/PlaylistCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PlaylistCollectionViewCell.swift
+//  arpeggio
+//
+//  Created by Jihoun Im on 11/15/21.
+//
+
+import UIKit
+
+class PlaylistCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
